ba58e134dfc49797af58e8f688772fc2bdad0287
Vincent MARTOWICZ <vmartowicz@gmail.com>
2025-07-05 15:02:38 +0200

    feat(fizzbuzz): step-10 assert 5=Buzz

--- fizz-buzz/src/main/java/fr/vmz/kata/fizzbuzz/FizzBuzz.java
+++ fizz-buzz/src/main/java/fr/vmz/kata/fizzbuzz/FizzBuzz.java
@@ -5,6 +5,9 @@ public class FizzBuzz {
     public static final int FIZZ = 3;
 
     public static String of(int i) {
+        if (i % 5 == 0) {
+            return "Buzz";
+        }
         if (isFizz(i)) {
             return "Fizz";
         }
