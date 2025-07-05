0926164c26a850ef19b933c355acab3b8bdb78fd
Vincent MARTOWICZ <vmartowicz@gmail.com>
2025-07-05 23:21:49 +0200

    feat(romannumerals): step-13 assert 18==XVIII - refactor

--- roman-numerals/src/main/java/fr/vmz/kata/romannumerals/RomanConverter.java
+++ roman-numerals/src/main/java/fr/vmz/kata/romannumerals/RomanConverter.java
@@ -8,15 +8,28 @@ public class RomanConverter {
 
     public static String convertToRoman(int arabic) {
         String roman = "";
-        if (arabic >= 10) {
-            roman += "X";
-            arabic -= 10;
-        }
-        if (arabic >= 5) {
-            roman += "V";
-            arabic -= 5;
+
+        for(RomanToDecimal romanToDecimal : RomanToDecimal.values()) {
+            if (arabic >= romanToDecimal.arabic) {
+                roman += romanToDecimal.roman;
+                arabic -= romanToDecimal.arabic;
+            }
         }
          roman += "I".repeat(Math.max(0, arabic));
         return roman;
     }
+
+    enum RomanToDecimal {
+        TEN("X", 10),
+        FIVE("V", 5);
+        private final String roman;
+        private final int arabic;
+
+        RomanToDecimal(String roman, int arabic){
+            this.roman = roman;
+            this.arabic = arabic;
+
+        }
+    }
+
 }
