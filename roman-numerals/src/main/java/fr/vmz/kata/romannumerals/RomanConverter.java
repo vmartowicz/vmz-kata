acf88e73fde734e5a44d78ef898623e05bdf2a78
Vincent MARTOWICZ <vmartowicz@gmail.com>
2025-07-05 23:05:48 +0200

    feat(romannumerals): step-09 assert 5==V

--- roman-numerals/src/main/java/fr/vmz/kata/romannumerals/RomanConverter.java
+++ roman-numerals/src/main/java/fr/vmz/kata/romannumerals/RomanConverter.java
@@ -7,6 +7,9 @@ public class RomanConverter {
     }
 
     public static String convertToRoman(int arabic) {
+        if (arabic == 5) {
+            return "V";
+        }
         return "I".repeat(Math.max(0, arabic));
     }
 }
