a2367f1c0b4872b7bb1b289a3675a2dc785dff26
Vincent MARTOWICZ <vmartowicz@gmail.com>
2025-07-05 14:56:12 +0200

    feat(fizzbuzz): step-07 assert 3=Fizz - refactor

--- fizz-buzz/src/main/java/fr/vmz/kata/fizzbuzz/FizzBuzz.java
+++ fizz-buzz/src/main/java/fr/vmz/kata/fizzbuzz/FizzBuzz.java
@@ -2,11 +2,15 @@ package fr.vmz.kata.fizzbuzz;
 
 public class FizzBuzz {
     public static String of(int i) {
-        if (i % 3 == 0) {
+        if (isFizz(i)) {
             return "Fizz";
         }
         if (i == 2)
             return "2";
         return "1";
+    }
+
+    private static boolean isFizz(int i) {
+        return i % 3 == 0;
     }
 }
